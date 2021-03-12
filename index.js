c8fd803f150d6bdc04038105c72e8ccc5dcd2b84
Ryan Roman <rtroman14@gmail.com>
2021-03-12 10:56:37 -0600

    merge with update

--- index.js
+++ index.js
@@ -13,27 +13,6 @@ const numContacts = 50;
                     isSuccessful ? (user.status = true) : (user.status = false);
                 }
             }
-<<<<<<< HEAD
-            if (users.integrity.status) {
-                const isSuccessful = await textContact(users.integrity);
-                isSuccessful ? (users.integrity.status = true) : (users.integrity.status = false);
-            }
-            if (users.allArea.status) {
-                const isSuccessful = await textContact(users.allArea);
-                isSuccessful ? (users.allArea.status = true) : (users.allArea.status = false);
-            }
-            if (users.guardian.status) {
-                const isSuccessful = await textContact(users.guardian);
-                isSuccessful ? (users.guardian.status = true) : (users.guardian.status = false);
-            }
-            // if (users.titan.status) {
-            //     const isSuccessful = await textContact(users.titan);
-            //     isSuccessful ? (users.titan.status = true) : (users.titan.status = false);
-            // }
-
-            // users.customInstallations.status && (await runCampaign(users.customInstallations));
-=======
->>>>>>> update
 
             await minutesWait(2);
         }
