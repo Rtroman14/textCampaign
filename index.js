1a840543fa36853ad634a564732ad277112b77bb
Ryan Roman <rtroman14@gmail.com>
2021-05-03 08:09:43 -0600

    update

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const numContacts = 50;
         campaigns = campaignsDueToday(campaigns);
         campaigns = campaignsToRun(campaigns);
 
-        // campaigns = campaigns.filter((campaign) => campaign.Client === "Integrity Pro Roofing");
+        // campaigns = campaigns.filter((campaign) => campaign.Client === "Farha Roofing");
 
         for (let i = 0; i < numContacts; i++) {
             for (let campaign of campaigns) {
