ca405ba1d597b1c9c9cb53b64eed770a88c07419
Ryan Roman <rtroman14@gmail.com>
2021-05-04 10:32:54 -0600

    update slack channel notification

--- src/slackNotification.js
+++ src/slackNotification.js
@@ -4,7 +4,7 @@ const axios = require("axios");
 
 module.exports = async (text) => {
     // notify me about this in Slack
-    await axios.post(process.env.SLACK_AUTOMATIONEXPERTS_DM, {
+    await axios.post(process.env.SLACK_T_N_M_C, {
         text,
     });
 };
