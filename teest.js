ceeb6fb37594510c83255021b8ff3d02d1dac856
Rtroman14 <rtroman14@gmail.com>
2021-05-26 14:14:44 -0600

    switch to live if contacts

--- teest.js
+++
@@ -1,9 +0,0 @@
-let numContacts = 50;
-
-let checkContacts = numContacts - 1;
-
-for (let contact = 1; contact < numContacts; contact++) {
-    if (contact === checkContacts) {
-        console.log("YEASSS");
-    }
-}
