4c38075c168a85563070405a58ea2bc18bfc4834
Rtroman14 <rtroman14@gmail.com>
2021-11-05 12:37:04 -0600

    optimize

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const numContacts = 60;
         const getCampaigns = await Airtable.getCampaigns();
         let accounts = _.accountsToRun(getCampaigns);
 
+        accounts = accounts.filter((acc) => acc.Account === "Farha Roofing - Michael");
+
         await slackNotification("Launching texts...");
 
         for (let i = 1; i < numContacts + 1; i++) {
